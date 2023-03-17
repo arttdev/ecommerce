be87ee122d2bb91c4d50c14b3899285313c66f4f
Andres <arttdev99@gmail.com>
2023-03-17 00:06:54 -0500

    Busqueda, Flexbox oks y barra nav(sin responsive)

--- test.js
+++ test.js
@@ -3,7 +3,9 @@ const app = document.querySelector("#search");
 app.addEventListener("keyup", (e) => {
     const searchValue = e.target.value;
     console.log(searchValue);
-    const url = `https://api.mercadolibre.com/sites/MLA/search?q=${searchValue}`;
+    const country = "MCO";
+    const url = `https://api.mercadolibre.com/sites/${country}/search?q=${searchValue}`;
+    //const url = `https://api.mercadolibre.com/sites/MLA/search?q=${searchValue}`;
     console.log(url);
     
 
@@ -12,17 +14,18 @@ fetch(url)
     .then((data) => {
         const products = data.results.map((product) => {
             return `
-            
-            <div class="card" style="width: 18rem;">
-     
-            <img src="${product.thumbnail}" class="card-img-top" alt="360" 
+            <div class="producto">
+            <span class="tipo">$ ${product.price}</span>
 
-            <div class="card-body">
-                <h5 class="card-title">${product.title}</h5>
-                <p class="card-text">${product.price}</p>
-                <a href="#" class="btn btn-primary">Agregar</a>
-            </div>
-            </div>
+            <img src="${product.thumbnail}" decoding="async" width="284" height="213">	
+            <span class="descripcion">${product.title.length > 25 ? product.title.slice(0, 25) + "..." : product.title} 
+            </span>
+            <span class="precio">${product.price}</span> 
+            </div> 
+
+            
+     
+        
 
             `;
         }).join('');
@@ -31,4 +34,8 @@ fetch(url)
         
         })
 })
+
+// paginacion de productos
+
+
 
